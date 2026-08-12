677976b29c0d60d9ae1ad98f26d7b964976638e3
kevinmontanodev <kevinmontanodev@gmail.com>
2026-08-11 20:12:35 -0600

    styles: improve styles of nav item component

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export function NavItem({url, children, current}: {url:string, children: ReactNode, current:boolean}){
     return (
-        <Link href={url} className={`relative p-4 rounded-full flex justify-start gap-2 sm:items-center hover:bg-zinc-50 hover:text-indigo-600 transition-all ${current ? 'bg-zinc-100 text-indigo-700' : ''}`}>
+        <Link href={url} className={`relative p-4 py-3 rounded-full flex justify-start gap-2 sm:items-center transition-all duration-300 ${current ? 'bg-black text-white' : 'hover:bg-zinc-100 hover:text-zinc-900'}`}>
             {children}
         </Link>
     )

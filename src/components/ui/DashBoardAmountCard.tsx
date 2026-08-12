d0f5a80df02f6c92666a4365d4f0bae6c43c6a8f
kevinmontanodev <kevinmontanodev@gmail.com>
2026-08-11 20:11:47 -0600

    fix: clean dashboard amount card component

--- src/components/ui/DashBoardAmountCard.tsx
+++ src/components/ui/DashBoardAmountCard.tsx
@@ -11,9 +11,9 @@ export function DashboardAmountCard({amountData}:{amountData: AmountCardProps}){
     return (
         <CardWrapper styles="p-8">
             <div className="flex flex-col justify-center items-center gap-2">
-                <span className="text-zinc-600 text-md md:text-base">{amountData.label}</span>
-                <div className="text-3xl font-bold">
-                    <span>
+                <span className="text-zinc-600 text-sm md:text-base">{amountData.label}</span>
+                <div>
+                    <span className="text-xl md:text-3xl font-bold">
                         {formatAmount(amountData.amount)}
                     </span>
                 </div>

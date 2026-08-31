2b23b3ed9b2f337053fc8dc94a425f55141f0b38
kevinmontanodev <kevinmontanodev@gmail.com>
2026-08-30 18:55:40 -0600

    feat: add account constants

---
+++ src/features/catalog/const/account.const.ts
@@ -0,0 +1,10 @@
+export const filterButtons = ['all', 'Activo','Pasivo', 'Gasto', 'Patrimonio', 'Ingreso']
+
+export const filersAccountItems = [
+    {id: 'all', label: 'Todos'},
+    {id: 'Activo', label: 'Activo'},
+    {id: 'Pasivo', label: 'Pasivo'},
+    {id: 'Patrimonio', label: 'Patrimonio'},
+    {id: 'Ingreso', label: 'Ingreso'},
+    {id: 'Gasto', label: 'Gasto'},
+]
